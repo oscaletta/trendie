fb7fdbdcf7fba99f6fb3341d273041a26eb3a9c3
oscaletta <altstronaut@gmail.com>
2021-02-24 20:41:59 -0600

    using chakra instead of mateirial to create the card

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from "react-dom";
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from "@material-ui/core/styles";
-//import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import ButtonBase from "@material-ui/core/ButtonBase";
 import axios from 'axios';
-import SearchBar from './components/searchbar';
 import Header from './components/header';
-import { theme, ChakraProvider, Grid, GridItem, Box, Image } from "@chakra-ui/react";
+import { theme, ChakraProvider, Box, Image, Badge, StarIcon } from "@chakra-ui/react";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: "auto",
-    maxWidth: 500
-  },
-  image: {
-    width: 128,
-    height: 128
-  },
-  img: {
-    margin: "auto",
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%"
-  }
-}));
+
 
 const breakpoints = ["360px", "768px", "1024px", "1440px"];
 breakpoints.sm = breakpoints[0];
@@ -45,7 +19,7 @@ const newTheme = {
 
 function App() {
 
-  const classes = useStyles();
+  
   const [tokenData, setTokenData] = useState('');
   const [priceData, setPriceData] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
@@ -79,66 +53,55 @@ function App() {
   console.log(input);
 
 
+  const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of modern home with pool",
+    beds: 3,
+    baths: 2,
+    title: "Modern home in city center in the heart of historic Los Angeles",
+    formattedPrice: "$1,900.00",
+    reviewCount: 34,
+    rating: 4,
+  }
+
+
   return (
     <>
       <ChakraProvider theme={newTheme} resetCSS>
         <Header />
-        <Paper>
-          <Grid
-          templateRows="repeat(1, 1fr)"
-          templateColumns="repeat(8, 1fr)"
-          gap={1}
-          >
-            <GridItem rowSpan={2} colSpan={1} bg="tomato">
-              <Box
-                w="150px"
-                borderRadius="10px"
-                overflow="hidden"
-                _hover={{ bg: "lightgrey" }}
-              >
-                <Image
-                  src={tokenData && tokenData.image && tokenData.image.small}
-                  alt={tokenData.name}
-                  height="155px"
-                  width="155px"
-                  objectFit="cover"
-                  display="block"
-                />
-                <Box
-                  as="h4"
-                  h="40px"
-                  mb="auto"
-                  fontWeight="bold"
-                  lineHeight="tight"
-                  display="flex"
-                  justifyContent="center"
-                  color="black"
-                  isTruncated
-                >
-                  {tokenData.name}
-                </Box>
-              </Box>
-            </GridItem>
-            <GridItem colSpan={3} bg="tomato">
-              <Typography gutterBottom variant="subtitle1">
-                {isLoading ? (<div>Loading ...</div>) : (tokenData.name)}
-              </Typography>
-              <Typography variant="body2" gutterBottom>
-                Soramitsu
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                ID: 1030114
-              </Typography>
-              <Typography variant="subtitle1">{isLoading ? ('Loading ...') : (priceData && priceData.usd )}</Typography>
-            </GridItem>
-            {/*<GridItem colSpan={3} bg="tomato" />*/}
-          </Grid>
-        </Paper>
-        
+        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Image src={tokenData && tokenData.image && tokenData.image.small} alt={tokenData && tokenData.image && tokenData.image.small} />
+          <Box p="6">
+                  <Badge borderRadius="full" px="2" colorScheme="teal">
+                    New
+                  </Badge>
+
+                  <Box color="gray.500" fontWeight="semibold" letterSpacing="wide"
+                    fontSize="xs" textTransform="uppercase" ml="2">
+                    {tokenData.name} beds &bull; {tokenData.name} baths
+                  </Box>
+
+                  <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight"
+                    isTruncated>
+                    {tokenData.name}
+                  </Box>
+                  
+                  <Box>
+                    {priceData && priceData.usd }
+                    <Box as="span" color="gray.600" fontSize="sm">
+                      / wk
+                    </Box>
+                  </Box>
+
+                  <Box d="flex" mt="2" alignItems="center"> 
+                    
+                    <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                      {priceData && priceData.usd} reviews
+                    </Box>
+                  </Box>
+          </Box>        
+        </Box>
       </ChakraProvider>
-      {/*<SearchBar input={input} setInput={setInput}/>
-      <button size="xl" variant="with-shadow">Get data</button>*/}
-      
     </>
   );
 }
